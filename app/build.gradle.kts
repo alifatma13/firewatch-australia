c59903c16824ed923702fee56291c4b8546e5fd8
Fatma Ali <alifatma13@gmail.com>
2026-03-10 17:39:11 +1100

    gradle path changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.alifatma.firewatch"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
+        versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
