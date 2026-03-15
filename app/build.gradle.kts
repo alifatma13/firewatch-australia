a4de17dde5c975fee9dd5fe262fe962d8c718752
Fatma Ali <alifatma13@gmail.com>
2026-03-15 12:48:40 +1100

    setup Retrofit networking with Kotlinx Serialization and OkHttp logging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.retrofit)
+    implementation(libs.logging.interceptor)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.retrofit2.kotlinx.serialization.converter)
 }
