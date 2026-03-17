b5bdd101873b79ec2101f521dedf58c9d9ec36f9
Fatma Ali <alifatma13@gmail.com>
2026-03-17 23:00:51 +1100

    Added a test to check the api response

---
+++ app/src/androidTest/java/com/alifatma/firewatch/RfsInstrumentedTest.kt
@@ -0,0 +1,32 @@
+package com.alifatma.firewatch
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.alifatma.firewatch.network.RetrofitClient
+import com.alifatma.firewatch.network.RfsApiService
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import retrofit2.Retrofit
+
+@RunWith(AndroidJUnit4::class)
+class RfsInstrumentedTest {
+
+    private lateinit var api : RfsApiService
+
+    @Before
+    fun setUp() {
+        api = RetrofitClient.api
+    }
+
+    @Test
+    fun testGetFires() {
+        runBlocking {
+            val response = api.getMajorIncidents()
+            assert(response.features.isNotEmpty())
+            val incidents = response.features.map { it.properties }
+            assert(incidents.isNotEmpty())
+        }
+
+    }
+}
