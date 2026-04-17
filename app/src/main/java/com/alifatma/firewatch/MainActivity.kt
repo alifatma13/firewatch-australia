ee1746ccd22030fb1d8140d682c07cd662d61cb2
Fatma Ali <alifatma13@gmail.com>
2026-04-17 20:02:33 +1000

    Dependencies and theme added

--- app/src/main/java/com/alifatma/firewatch/MainActivity.kt
+++ app/src/main/java/com/alifatma/firewatch/MainActivity.kt
@@ -1,28 +1,31 @@
 package com.alifatma.firewatch
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.alifatma.firewatch.ui.MainViewModel
+import com.alifatma.firewatch.ui.screens.MainScreen
+import com.alifatma.firewatch.ui.theme.FireWatchTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
     private val viewModel : MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContent {
+            FireWatchTheme {
+                MainScreen(viewModel = viewModel)
+            }
 
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
 
     }
